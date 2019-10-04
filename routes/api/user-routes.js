c01ff6fb2700ebc34bdaec88bd20fd4642bc854c
Mvcampbell3 <mvcampbell3@gmail.com>
2019-10-04 01:25:02 -0400

    basic user model for front-end testing

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const db = require("../../models");
 
 router.use('*', (req, res, next) => {
   console.log('made it to user folder');
@@ -7,6 +8,21 @@ router.use('*', (req, res, next) => {
 
 router.get('/', (req, res) => {
   res.json({ user: true })
+})
+
+router.get('/all', (req, res) => {
+  db.User.find()
+    .then(users => res.status(200).json({ users }))
+    .catch(err => res.status(500).json(err))
+})
+
+router.post('/signup', (req, res) => {
+  db.User.findOne({email: req.body.email})
+    .then(dbUser => {
+      if (!dbUser) {
+        
+      }
+    })
 })
 
 module.exports = router;
