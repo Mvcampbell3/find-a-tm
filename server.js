27d09350f6ad1be3dc3d2dc1e1e4d98d79e39b35
Mvcampbell3 <mvcampbell3@gmail.com>
2019-10-16 18:12:50 -0400

    including express static

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express()
 const mongoose = require('mongoose');
 const PORT = process.env.PORT || 3001;
 const routes = require('./routes');
+const path = require('path')
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
@@ -13,6 +14,8 @@ require('dotenv').config()
 //   console.log(req.originalUrl);
 //   next()
 // })
+
+app.use(express.static(path.join(__dirname, "client/dist/client")))
 
 app.use(routes)
 
