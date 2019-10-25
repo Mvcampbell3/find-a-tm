d3c016b181acf0497bdcb669a35687e9bbdb57a6
Mvcampbell3 <mvcampbell3@gmail.com>
2019-10-25 17:55:26 -0400

    changed back to original local mongo and set seed to false

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3001;
 const routes = require('./routes');
 const path = require('path')
 const gameSeeds = require('./seeds/gameSeed');
-const seedDB = true;
+const seedDB = false;
 const db = require('./models');
 
 app.use(express.urlencoded({ extended: true }));
@@ -22,25 +22,26 @@ app.use(express.static(path.join(__dirname, "client/dist/client")))
 
 app.use(routes)
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/teammatefindertest",
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/teammatefinder",
   { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
   .then(() => {
     console.log('mongodb connected');
     if (seedDB) {
-      gameSeeds.forEach(game => {
-        const newGame = new db.Game({
-          title: game.title,
-          img_url: game.img_url,
-          developer: game.developer,
-          team_game: game.team_game,
-          reviewed: game.reviewed,
-          userId: game.userId,
-          ps4: game.ps4
-        })
-
-        newGame.save()
-
-      })
+      db.Game.remove()
+        .then(
+          gameSeeds.forEach(game => {
+            const newGame = new db.Game({
+              title: game.title,
+              img_url: game.img_url,
+              developer: game.developer,
+              team_game: game.team_game,
+              reviewed: game.reviewed,
+              userId: game.userId,
+              ps4: game.ps4
+            })
+            newGame.save()
+          })
+        )
     }
     app.listen(PORT, () => {
       console.log(`server is live on http://localhost:${PORT}`)
