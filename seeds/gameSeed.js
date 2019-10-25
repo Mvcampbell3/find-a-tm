0b2bacdf594d01f189fa49cc8e84fdd4d51f55e6
Mvcampbell3 <mvcampbell3@gmail.com>
2019-10-25 17:41:54 -0400

    added test seed for heroku db

---
+++ seeds/gameSeed.js
@@ -0,0 +1,41 @@
+const UFC = {
+  title: 'UFC 3',
+  developer: 'EASports',
+  ps4: true,
+  team_game: false,
+  img_url: 'https://b.thumbs.redditmedia.com/VOiVgeo3FqOo8uBCttmGviQnj9vQhC6BTa6PB64IiCk.png',
+  reviewed: true,
+  userId: 'default'
+}
+
+const RocketLeague = {
+  title: 'Rocket League',
+  developer: 'Psyonix',
+  ps4: true,
+  team_game: true,
+  img_url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSa7E7wykeEga3VaXlONx1S0ZiGRLqOZMoz5Yk4REThDrBN6GWvpw',
+  reviewed: true,
+  userId: 'default'
+}
+
+const Hitman2 = {
+  title: 'Hitman 2',
+  developer: 'IO Interactive',
+  ps4: true,
+  team_game: true,
+  img_url: 'https://tpucdn.com/review/hitman-2-benchmark-performance-test/images/small-v1542925931.png',
+  reviewed: true,
+  userId: 'default'
+}
+
+const Fortnite = {
+  title: 'Fortnite',
+  developer: 'Epic Games',
+  ps4: true,
+  team_game: true,
+  img_url: 'https://i.imgur.com/qxm2OAQ.jpg',
+  reviewed: true,
+  userId: 'default'
+}
+
+module.exports = [UFC, Hitman2, RocketLeague, Fortnite];
--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const mongoose = require('mongoose');
 const PORT = process.env.PORT || 3001;
 const routes = require('./routes');
 const path = require('path')
+const gameSeeds = require('./seeds/gameSeed');
+const seedDB = false;
+const db = require('./models');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
@@ -23,6 +26,22 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/teammatefinder"
   { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
   .then(() => {
     console.log('mongodb connected');
+    if (seedDB) {
+      gameSeeds.forEach(game => {
+        const newGame = db.newGame({
+          title: game.title,
+          img_url: game.img_url,
+          developer: game.developer,
+          team_game: game.team_game,
+          reviewed: game.reviewed,
+          userId: game.userId,
+          ps4: game.ps4
+        })
+
+        newGame.save()
+
+      })
+    }
     app.listen(PORT, () => {
       console.log(`server is live on http://localhost:${PORT}`)
     })
